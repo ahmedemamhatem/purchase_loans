3e492d9fb9d15c9b1a8a484875cf78ad11ab46df
Ahmed Emam <aemam@creativeadvtech.com>
2025-01-13 12:56:07 +0000

    don't duplicate the sut

--- purchase_loans/purchase_loans/doctype/purchase_loan_request/purchase_loan_request.js
+++ purchase_loans/purchase_loans/doctype/purchase_loan_request/purchase_loan_request.js
@@ -167,27 +167,50 @@ function pay_to_employee(frm, mode_of_payment, payment_amount, payment_date) {
 
 // Function for creating Settlement
 function create_settlement(frm) {
+    // Check if a Purchase Loan Repayment with the same purchase_loan_request exists in draft status
     frappe.call({
-        method: "frappe.client.insert",
+        method: "frappe.client.get_list",
         args: {
-            doc: {
-                doctype: "Purchase Loan Repayment",
-                employee: frm.doc.employee,
-                loan_request: frm.doc.name,
-                loan_amount: frm.doc.paid_amount_from_request,
-                outstanding_amount: frm.doc.outstanding_amount_from_repayment,
-                company: frm.doc.company,
-                direct_approver: frm.doc.direct_approver,
-                direct_approver_name: frm.doc.direct_approver_name,
-                default_account: frm.doc.default_account,
-                purchase_loan_request: frm.doc.name
-            }
+            doctype: "Purchase Loan Repayment",
+            filters: {
+                purchase_loan_request: frm.doc.name,
+                docstatus: 0 // Draft status
+            },
+            fields: ["name"]
         },
         callback: function(response) {
-            frappe.set_route('Form', 'Purchase Loan Repayment', response.message.name);
+            if (response.message && response.message.length > 0) {
+                // If a draft exists, redirect to the existing record
+                frappe.set_route('Form', 'Purchase Loan Repayment', response.message[0].name);
+                
+            } else {
+                // No draft record exists, proceed with creation
+                frappe.call({
+                    method: "frappe.client.insert",
+                    args: {
+                        doc: {
+                            doctype: "Purchase Loan Repayment",
+                            employee: frm.doc.employee,
+                            loan_request: frm.doc.name,
+                            loan_amount: frm.doc.paid_amount_from_request,
+                            outstanding_amount: frm.doc.outstanding_amount_from_repayment,
+                            company: frm.doc.company,
+                            direct_approver: frm.doc.direct_approver,
+                            direct_approver_name: frm.doc.direct_approver_name,
+                            default_account: frm.doc.default_account,
+                            purchase_loan_request: frm.doc.name
+                        }
+                    },
+                    callback: function(response) {
+                        frappe.set_route('Form', 'Purchase Loan Repayment', response.message.name);
+                    }
+                });
+            }
         }
     });
 }
+
+
 
 // Function for showing Repay Cash dialog
 function show_repay_cash_dialog(frm) {
