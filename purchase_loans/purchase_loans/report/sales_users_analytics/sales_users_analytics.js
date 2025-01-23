c1894d3994059117e13c5328068b90c4e9d84e2a
Ahmed Emam <aemam@creativeadvtech.com>
2025-01-23 14:06:34 +0000

    add Sales Analyses report01

--- purchase_loans/purchase_loans/report/sales_users_analytics/sales_users_analytics.js
+++ purchase_loans/purchase_loans/report/sales_users_analytics/sales_users_analytics.js
@@ -1,49 +1,48 @@
-// Copyright (c) 2025, Ahmed Emam and contributors
-// For license information, please see license.txt
-
-// frappe.query_reports["Sales Users Analytics"] = {
-// 	"filters": [
-
-// 	]
-// };
-
 frappe.query_reports["Sales Users Analytics"] = {
     filters: [
         {
             fieldname: "from_date",
             label: __("From Date"),
             fieldtype: "Date",
-            default: frappe.datetime.add_days(frappe.datetime.get_today(), -30), // Default: Last 30 days
-            reqd: 1
+            default: frappe.datetime.add_days(frappe.datetime.get_today(), -30),
+            reqd: 1,
         },
         {
             fieldname: "to_date",
             label: __("To Date"),
             fieldtype: "Date",
-            default: frappe.datetime.get_today(), // Default: Today's date
-            reqd: 1
+            default: frappe.datetime.get_today(),
+            reqd: 1,
         },
-		{
+        {
             fieldname: "creator",
             label: __("Creator"),
             fieldtype: "Link",
             options: "User",
-            default: frappe.session.user, // Set default to the current logged-in user (optional)
-            reqd: 0
         },
         {
             fieldname: "item_code",
             label: __("Item Code"),
             fieldtype: "Link",
             options: "Item",
-            reqd: 0
         },
         {
             fieldname: "customer",
             label: __("Customer"),
             fieldtype: "Link",
             options: "Customer",
-            reqd: 0
-        }
-    ]
+        },
+        {
+            fieldname: "group_by_customer",
+            label: __("Group by Customer"),
+            fieldtype: "Check",
+            default: 0, // Default is unchecked
+        },
+        {
+            fieldname: "group_by_item",
+            label: __("Group by Item"),
+            fieldtype: "Check",
+            default: 0, // Default is unchecked
+        },
+    ],
 };
--- purchase_loans/purchase_loans/report/sales_users_analytics/sales_users_analytics.py
+++ purchase_loans/purchase_loans/report/sales_users_analytics/sales_users_analytics.py
@@ -1,26 +1,32 @@
 import frappe
-from frappe.utils import flt, nowdate
+from frappe.utils import flt
 
 def execute(filters=None):
-    # Define columns
-    columns = get_columns()
+    # Define columns based on filters
+    columns = get_columns(filters)
 
     # Get data based on filters
     data = get_data(filters)
 
     return columns, data
 
-def get_columns():
-    return [
-       
+def get_columns(filters):
+    columns = [
         {"fieldname": "creator_full_name", "label": "Creator Full Name", "fieldtype": "Data", "width": 200},
-        {"fieldname": "customer", "label": "Customer", "fieldtype": "Data", "width": 200},
-        {"fieldname": "item_code", "label": "Item Code", "fieldtype": "Link", "options": "Item", "width": 200},
-        {"fieldname": "item_name", "label": "Item Name", "fieldtype": "Data", "width": 200},
         {"fieldname": "qty_with_rate", "label": "Qty with Rate", "fieldtype": "Float", "width": 200},
         {"fieldname": "qty_free", "label": "Qty Free (Rate = 0)", "fieldtype": "Float", "width": 200},
         {"fieldname": "value", "label": "Total Value", "fieldtype": "Currency", "options": "currency", "width": 200},
     ]
+
+    # Dynamically add columns based on grouping filters
+    if filters.get("group_by_customer"):
+        columns.insert(1, {"fieldname": "customer", "label": "Customer", "fieldtype": "Data", "width": 200})
+
+    if filters.get("group_by_item"):
+        columns.insert(1, {"fieldname": "item_code", "label": "Item Code", "fieldtype": "Link", "options": "Item", "width": 200})
+        columns.insert(2, {"fieldname": "item_name", "label": "Item Name", "fieldtype": "Data", "width": 200})
+
+    return columns
 
 def get_data(filters):
     conditions = []
@@ -35,7 +41,7 @@ def get_data(filters):
         conditions.append("soi.item_code = %(item_code)s")
         values["item_code"] = filters["item_code"]
 
-    if filters.get("customer"):
+    if filters.get("customer") and filters.get("group_by_customer"):
         conditions.append("so.customer = %(customer)s")
         values["customer"] = filters["customer"]
 
@@ -46,17 +52,39 @@ def get_data(filters):
 
     condition_str = " AND ".join(conditions) if conditions else "1=1"
 
+    # Determine the GROUP BY clause dynamically
+    group_by_fields = ["so.owner", "u.full_name"]
+    if filters.get("group_by_customer"):
+        group_by_fields.append("so.customer")
+    if filters.get("group_by_item"):
+        group_by_fields.append("soi.item_code")
+        group_by_fields.append("soi.item_name")
+
+    group_by_clause = ", ".join(group_by_fields)
+
+    # Select fields dynamically based on grouping filters
+    select_fields = [
+        "so.owner AS creator",
+        "u.full_name AS creator_full_name",
+    ]
+    if filters.get("group_by_customer"):
+        select_fields.append("so.customer AS customer")
+    if filters.get("group_by_item"):
+        select_fields.append("soi.item_code AS item_code")
+        select_fields.append("soi.item_name AS item_name")
+
+    select_fields.extend([
+        "SUM(CASE WHEN soi.rate > 0 THEN soi.qty ELSE 0 END) AS qty_with_rate",
+        "SUM(CASE WHEN soi.rate = 0 THEN soi.qty ELSE 0 END) AS qty_free",
+        "SUM(soi.amount) AS value"
+    ])
+
+    select_clause = ", ".join(select_fields)
+
     # Query data
     query = f"""
         SELECT
-            so.owner AS creator,
-            u.full_name AS creator_full_name,
-            so.customer AS customer,
-            soi.item_code AS item_code,
-            soi.item_name AS item_name,
-            SUM(CASE WHEN soi.rate > 0 THEN soi.qty ELSE 0 END) AS qty_with_rate,
-            SUM(CASE WHEN soi.rate = 0 THEN soi.qty ELSE 0 END) AS qty_free,
-            SUM(soi.amount) AS value
+            {select_clause}
         FROM
             `tabSales Order` so
         INNER JOIN
@@ -66,8 +94,8 @@ def get_data(filters):
         WHERE
             so.docstatus = 1 AND {condition_str}
         GROUP BY
-            so.owner, u.full_name, so.customer, soi.item_code
+            {group_by_clause}
         ORDER BY
-            so.owner, so.customer, soi.item_code
+            so.owner, soi.item_code
     """
     return frappe.db.sql(query, values, as_dict=True)
